d2ceb5e620ab60975acecbdbddae454a2da9d82a
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2023-11-25 18:15:28 -0300

    adding gpio init function

--- lab04/gpio.c
+++ lab04/gpio.c
@@ -6,17 +6,17 @@
 #include "tm4c1294ncpdt.h"
 
 // Definições dos Ports
-#define GPIO_PORTA (0x00000001)	// SYSCTL_PPGPIO_P0
+#define GPIO_PORTE (0x00000010)	// SYSCTL_PPGPIO_P4
 #define GPIO_PORTF (0x00000020)	// SYSCTL_PPGPIO_P5
-#define GPIO_PORTH (0x00000080)	// SYSCTL_PPGPIO_P7
 #define GPIO_PORTJ (0x00000100)	// SYSCTL_PPGPIO_P8
-#define GPIO_PORTN (0x00001000)	// SYSCTL_PPGPIO_P12
+#define GPIO_PORTK (0x00000200)	// SYSCTL_PPGPIO_P9
+#define GPIO_PORTL (0x00000400)	// SYSCTL_PPGPIO_P10
+#define GPIO_PORTM (0x00000800)	// SYSCTL_PPGPIO_P11
 #define GPIO_PORTP (0x00002000)	// SYSCTL_PPGPIO_P13
-#define GPIO_PORTQ (0x00004000)	// SYSCTL_PPGPIO_P14
 
 // -------------------------------------------------------------------------------
 // Função GPIO_Init
-// Inicializa os ports A, F, H, J, N, P e Q
+// Inicializa os ports E, F, J, K, L, M e P
 // Parâmetro de entrada: Não tem
 // Parâmetro de saída: Não tem
 void GPIO_Init(void)
@@ -24,78 +24,60 @@ void GPIO_Init(void)
 	// 1. Habilitar o clock no módulo GPIO no registrador RCGGPIO (cada bit representa uma GPIO) e
 	// esperar até que a respectiva GPIO esteja pronta para ser acessada no registrador PRGPIO (cada
 	// bit representa uma GPIO).
-	SYSCTL_RCGCGPIO_R = (GPIO_PORTA | GPIO_PORTF | GPIO_PORTH | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTP | GPIO_PORTQ);
+	SYSCTL_RCGCGPIO_R = (GPIO_PORTE | GPIO_PORTF | GPIO_PORTJ | GPIO_PORTK | GPIO_PORTL | GPIO_PORTM | GPIO_PORTP);
 
-  while((SYSCTL_PRGPIO_R & (GPIO_PORTA | GPIO_PORTF | GPIO_PORTH | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTP | GPIO_PORTQ) ) != 
-													 (GPIO_PORTA | GPIO_PORTF | GPIO_PORTH | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTP | GPIO_PORTQ) )
+  while((SYSCTL_PRGPIO_R & (GPIO_PORTE | GPIO_PORTF | GPIO_PORTJ | GPIO_PORTK | GPIO_PORTL | GPIO_PORTM | GPIO_PORTP) ) != 
+													 (GPIO_PORTE | GPIO_PORTF | GPIO_PORTJ | GPIO_PORTK | GPIO_PORTL | GPIO_PORTM | GPIO_PORTP) )
 	{
 		//
 	};
 	
 	// 2. Desabilitar a funcionalidade analógica no registrador GPIOAMSEL.
-	GPIO_PORTA_AHB_AMSEL_R = 0x00;
+	GPIO_PORTE_AHB_AMSEL_R = 0x10;		// 0x10 = 2_10000: PE4 analógica
 	GPIO_PORTF_AHB_AMSEL_R = 0x00;
-	GPIO_PORTH_AHB_AMSEL_R = 0x00;
 	GPIO_PORTJ_AHB_AMSEL_R = 0x00;
-	GPIO_PORTN_AMSEL_R = 0x00;
-	GPIO_PORTP_AMSEL_R = 0x00;
-	GPIO_PORTQ_AMSEL_R = 0x00;				
+	GPIO_PORTK_AMSEL_R = 0x00;
+	GPIO_PORTL_AMSEL_R = 0x00;
+	GPIO_PORTM_AMSEL_R = 0x00;				
+	GPIO_PORTP_AMSEL_R = 0x00;	
 		
 	// 3. Limpar PCTL para selecionar o GPIO
-	GPIO_PORTA_AHB_PCTL_R = 0x11;				// UART0
+	GPIO_PORTE_AHB_PCTL_R = 0x00;
 	GPIO_PORTF_AHB_PCTL_R = 0x00;
-	GPIO_PORTH_AHB_PCTL_R = 0x00;
 	GPIO_PORTJ_AHB_PCTL_R = 0x00;
-	GPIO_PORTN_PCTL_R = 0x00;
+	GPIO_PORTK_PCTL_R = 0x00;
+	GPIO_PORTL_PCTL_R = 0x00;
+	GPIO_PORTM_PCTL_R = 0x00;
 	GPIO_PORTP_PCTL_R = 0x00;
-	GPIO_PORTQ_PCTL_R = 0x00;
 															
 	// 4. DIR para 0 se for entrada, 1 se for saída
-	GPIO_PORTA_AHB_DIR_R = 0xF2;				// 11110010	: PA7 ao PA4 e PA1
-	GPIO_PORTF_AHB_DIR_R = 0x11;				// 10001		: PF3 ao PF1
-	GPIO_PORTH_AHB_DIR_R = 0x0F;				// 1111			: PH3 ao PH0 (driver de potência)
-	GPIO_PORTJ_AHB_DIR_R = 0x00;				// 00				: PJ0
-	GPIO_PORTN_DIR_R = 0x03;						// 11				: PN1 ao PN0
-	GPIO_PORTP_DIR_R = 0x20;						// 100000		: PP5
-	GPIO_PORTQ_DIR_R = 0x0F;						// 1111			: PQ3 ao PQ0
+	GPIO_PORTE_AHB_DIR_R = 0x03;			// 0x03 = 2_00000011: PE1 e PE0
+	GPIO_PORTF_AHB_DIR_R = 0x04;			// 0x04 = 2_00000100: PF2
+	GPIO_PORTJ_AHB_DIR_R = 0x00;			// 0x00 = 2_00000000
+	GPIO_PORTK_DIR_R = 0xFF;					// 0xFF = 2_11111111: PK7:PK0
+	GPIO_PORTL_DIR_R = 0x00;					// 0x00 = 2_00000000
+	GPIO_PORTM_DIR_R = 0xF7;					// 0xF7 = 2_11110111: PM7:PM4 e PM2:PM0
+	GPIO_PORTP_DIR_R = 0x02;					// 0x02 = 2_00000010: PP1
 		
 	// 5. Limpar os bits AFSEL para selecionar GPIO sem função alternativa	
-	GPIO_PORTA_AHB_AFSEL_R = 0x03;			// UART
+	GPIO_PORTE_AHB_AFSEL_R = 0x10;		// 0x10 = 2_10000: PE4 analógica
 	GPIO_PORTF_AHB_AFSEL_R = 0x00;
-	GPIO_PORTH_AHB_AFSEL_R = 0x00;
 	GPIO_PORTJ_AHB_AFSEL_R = 0x00;
-	GPIO_PORTN_AFSEL_R = 0x00;
+	GPIO_PORTK_AFSEL_R = 0x00;
+	GPIO_PORTL_AFSEL_R = 0x00;
+	GPIO_PORTM_AFSEL_R = 0x00;
 	GPIO_PORTP_AFSEL_R = 0x00;
-	GPIO_PORTQ_AFSEL_R = 0x00;
 		
 	// 6. Setar os bits de DEN para habilitar I/O digital	
-	GPIO_PORTA_AHB_DEN_R = 0xF3;				// 11110011	: PA7 ao PA4 e PA1 ao PA0
-	GPIO_PORTF_AHB_DEN_R = 0x11;				// 10001		: PF3 ao PF1
-	GPIO_PORTH_AHB_DEN_R = 0x0F;				// 1111			: PH3 ao PH0 (driver de potência)
-	GPIO_PORTJ_AHB_DEN_R = 0x01;				// 1				: PJ0
-	GPIO_PORTN_DEN_R = 0x03;						// 11				: PN1 ao PN0
-	GPIO_PORTP_DEN_R = 0x20;						// 100000		: PP5
-	GPIO_PORTQ_DEN_R = 0x0F;						// 1111			: PQ3 ao PQ0
+	GPIO_PORTE_AHB_DEN_R = 0x03;		// 0x03 = 2_00000011: PE1 e PE0
+	GPIO_PORTF_AHB_DEN_R = 0x04;		// 0x04 = 2_00000100: PF3
+	GPIO_PORTJ_AHB_DEN_R = 0x03;		// 0x03 = 2_00000011: PJ1 e PJ0
+	GPIO_PORTK_DEN_R = 0xFF;				// 0xFF = 2_11111111: PK7:PK0
+	GPIO_PORTL_DEN_R = 0x0F;				// 0x0F = 2_00001111: PL3:PL0
+	GPIO_PORTM_DEN_R = 0xF7;				// 0xF7 = 2_11110111: PM7:PM4 e PM2:PM0
+	GPIO_PORTP_DEN_R = 0x03;				// 0x03 = 2_00000011: PP1 e PP0
 	
 	// 7. Habilitar resistor de pull-up interno, setar PUR para 1
-	GPIO_PORTJ_AHB_PUR_R = 0x01;				// PJ0
-	
-	// 8. Interrupções
-	GPIO_PORTJ_AHB_IM_R		= 0x00;
-	GPIO_PORTJ_AHB_IS_R		= 0x00;
-	GPIO_PORTJ_AHB_IBE_R	= 0x00;
-	GPIO_PORTJ_AHB_IEV_R	= 0x00;
-	GPIO_PORTJ_AHB_ICR_R	= 0x01;
-	GPIO_PORTJ_AHB_IM_R		= 0x01;
-	NVIC_EN1_R						= 0x80000;		// 2_1 << 19
-	NVIC_PRI12_R					= 0xA0000000;	// 2_5 << 29
-}
-
-// Função GPIOPortJ_Handler
-// Trata a interrupção no PJ0
-// Parâmetro de entrada: Não tem
-// Parâmetro de saída: Não tem
-void GPIOPortJ_Handler(void)
-{
-	GPIO_PORTJ_AHB_ICR_R = 1;	// Limpa a flag de interrupção
+	GPIO_PORTJ_AHB_PUR_R = 0x3;		// PJ1 e PJ0
+	GPIO_PORTL_PUR_R = 0xF;				// PL3:PL0
 }
