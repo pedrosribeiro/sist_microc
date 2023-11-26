f92997470e96453c831bc00739a21f644dbd8aa2
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2023-11-26 17:18:49 -0300

    adding lcd functions

--- lab04/lcd.c
+++ lab04/lcd.c
@@ -9,22 +9,74 @@ uint32_t PortL_Input (void);
 void PortK_Output (uint32_t data);
 void PortM_Output (uint32_t data);
 
+void LCD_Instruction (uint32_t inst);
+
+// Função LCD_Init
+// Inicializa o LCD
+// Parâmetro de entrada: Não tem
+// Parâmetro de saída: Não tem
 void LCD_Init (void)
 {
-	//
+	LCD_Instruction(0x38);	// Inicializar no modo 2 linhas/caracter matriz 5x7
+	LCD_Instruction(0x06);	// Cursor com autoincremento para direita
+	LCD_Instruction(0x0E);	// Configurar o cursor (habilitar o display + cursor + não-pisca)
+	LCD_Instruction(0x01);	// Resetar: Limpar o display e levar o cursor para o home
 }
 
+// Função LCD_Instruction
+// Escreve no barramento de dados no modo instrução
+// Parâmetro de entrada: Instrução a ser escrita
+// Parâmetro de saída: Não tem
+void LCD_Instruction (uint32_t inst)
+{
+	PortM_Output(0x04); 	// Ativa o modo de instrução (EN=1, RW=0, RS=0)
+	
+	PortK_Output(inst);		// Instrução
+	SysTick_Wait1ms(10); 	// Delay de 10ms para executar
+	
+	PortM_Output(0x00);		// Desativa o modo de instrução (EN=0, RW=0, RS=0)
+}
+
+// Função LCD_Data
+// Escreve no barramento de dados no modo de dados
+// Parâmetro de entrada: Dado a ser escrito
+// Parâmetro de saída: Não tem
+void LCD_Data (uint32_t data)
+{
+	PortM_Output(0x04); 	// Ativa o modo de dados (EN=1, RW=0, RS=1)
+	
+	PortK_Output(data);		// Dado
+	SysTick_Wait1ms(10); 	// Delay de 10ms para executar
+	
+	PortM_Output(0x00);		// Desativa o modo de dados (EN=0, RW=0, RS=0)
+}
+
+// Função LCD_Reset
+// Limpa o display e leva o cursor para o home
+// Parâmetro de entrada: Não tem
+// Parâmetro de saída: Não tem
 void LCD_Reset (void)
 {
-	//
+	LCD_Instruction(0x01);	// Resetar: Limpar o display e levar o cursor para o home
 }
 
-void LCD_WriteChar (uint32_t data)
+// Função LCD_Line2
+// Coloca o cursor no endereço da primeira posição - Segunda Linha
+// Parâmetro de entrada: Não tem
+// Parâmetro de saída: Não tem
+void LCD_Line2 (void)
 {
-	//
+	LCD_Instruction(0xC0);	// Endereço da primeira posição - Segunda Linha
 }
 
-void LCD_WriteString (char* data)
+// Função LCD_WriteString
+// Imprime uma string no LCD através de um loop
+// Parâmetro de entrada: String a ser escrita
+// Parâmetro de saída: Não tem
+void LCD_WriteString (char* str)
 {
-	//
+	for (int i = 0; i < strlen(str); i++)
+	{
+		LCD_Data(str[i]);
+	}
 }
--- lab04/lcd.h
+++ lab04/lcd.h
@@ -3,6 +3,8 @@
 #include <stdint.h>
 
 void LCD_Init (void);
+void LCD_Instruction (uint32_t inst);
+void LCD_Data (uint32_t data);
 void LCD_Reset (void);
-void LCD_WriteChar (uint32_t data);
-void LCD_WriteString (char* data);
+void LCD_Line2 (void);
+void LCD_WriteString (char* str);
